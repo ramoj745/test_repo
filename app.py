ae24d66ee466a97f1b666074331ffc0e411a273c
Ramoj <ramoj745@gmail.com>
2025-05-16 15:21:57 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# webhook.py
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    subprocess.run(['git', '-C', '/home/ramoj745/PSPACE', 'pull'])
+    subprocess.run(['sudo', 'systemctl', 'restart', 'your-flask-app'])
+    return 'Webhook received!', 200
+
+if __name__ == '__main__':
+    app.run(port=5001)  # or any unused port
