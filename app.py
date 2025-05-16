b2fb3735aabe6fde63025b2c6d72b5f6fdf1e873
Ramoj <ramoj745@gmail.com>
2025-05-16 15:40:53 +0800

    third commit

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+# # webhook.py
+# from flask import Flask, request
+# import subprocess
+
+# app = Flask(__name__)
+
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     subprocess.run(['git', '-C', '/home/ramoj745/test_repo', 'pull'])
+#     subprocess.run(['sudo', 'systemctl', 'restart', 'your-flask-app'])
+#     return 'Webhook received!', 200
+
+# if __name__ == '__main__':
+#     app.run(port=5001)  # or any unused port
+
 # webhook.py
 from flask import Flask, request
 import subprocess
@@ -7,7 +22,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     subprocess.run(['git', '-C', '/home/ramoj745/test_repo', 'pull'])
-    subprocess.run(['sudo', 'systemctl', 'restart', 'your-flask-app'])
+    subprocess.run(['sudo', 'systemctl', 'restart', 'app'])
     return 'Webhook received!', 200
 
 if __name__ == '__main__':
